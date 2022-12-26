5b50dd9e9e8773b19665722d9fb2dab4947bb067
Ridwan <ridwanhamzat99@gmail.com>
2022-12-26 11:51:20 +0100

    feat: add increase, decrease and remove item functionalities

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,14 +3,33 @@ import cartItems from '../../cartItems'
 
 const initialState = {
     cartItems: cartItems,
-    amount: 5,
+    amount: 4,
     total: 0,
     isLoading: true,
 }
 
 const cartSlice = createSlice({
     name: 'cart',
-    initialState
+    initialState,
+    reducers: {
+        clearCart: (state)=>{
+            state.cartItems = []
+        },
+        removeItem: (state, action) =>{
+            const itemId = action.payload;
+            state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+        },
+        increase: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount + 1;
+        },
+        decrease: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount - 1;
+        },
+    }
 })
+
+export const {clearCart, removeItem, decrease, increase} = cartSlice.actions
 
 export default cartSlice.reducer
